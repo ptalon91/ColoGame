edb8d9afdb0ddde2d9af1b7334dffa00d86b5ef6
Patrick Talon <p.talon@bluewin.ch>
2017-04-14 14:02:48 +0200

    Ajoute 3 tasks lors du démarrage du serveur
    Permet d'ajouter des tasks à la collection "tasks" si cette dernière est
    vide lors du démarrage. Utile pour le développement du site.

---
+++ server/tasksForDev.js
@@ -0,0 +1,31 @@
+if (Tasks.find().count() === 0) {
+  Tasks.insert({
+    name: 'passer la panosse',
+    descr: 'passé la panosse', 
+    points: '1200', 
+    service: true, 
+    gage: true, 
+    pending: false,
+    createdAt: new Date(),
+  });
+
+  Tasks.insert({
+      name: 'payer une bière',
+      descr: 'payé une bière', 
+      points: '300', 
+      service: false, 
+      gage: true, 
+      pending: false,
+      createdAt: new Date(),
+    });
+
+  Tasks.insert({
+      name: 'faire la vaisselle',
+      descr: 'fait la vaiselle', 
+      points: '1000', 
+      service: true, 
+      gage: true, 
+      pending: false,
+      createdAt: new Date(),
+    });
+}
