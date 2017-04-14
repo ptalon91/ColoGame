6386401162c44ad8c6161a114e63a66255524066
Patrick Talon <p.talon@bluewin.ch>
2017-04-14 15:23:03 +0200

    Suppression de "autopublish" et mise en place d'une publication des données
    autopublish renvoyait au client toutes nos données, ce qu'on ne souhaite
    pas forcément. Les publications (côté serveur) permettent d'envoyer
    uniquement les données souhaitées. Il faut ensuite créer une
    "subscription" dans main.js pour lire ces données côté client.

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+Meteor.subscribe('tasks');
+
 /*import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
