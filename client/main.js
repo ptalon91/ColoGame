dbab7fef553461dd74c9795f1ab6afd9c66568f9
Patrick Talon <p.talon@bluewin.ch>
2017-04-14 13:58:08 +0200

    Ajout de la collection tasks
    tasks.js contient une ligne de code pour créer la collection. On peu
    ensuite ajouter du contenu depuis le shell Mongo, ou à partir d'un
    fichier javascript.

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+/*import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -20,3 +20,4 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+*/
