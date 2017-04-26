156ca9d2b6629a6f5a32d3b8e0e954cfe8f871e5
nvallott <nvallott@hotmail.com>
2017-04-26 10:03:35 +0200

    add colocname

--- server/publications.js
+++ server/publications.js
@@ -4,11 +4,11 @@ Meteor.publish('tasks', function() {
 	return Tasks.find();
 });
 
-// Publication of the "points" field of the users. Now we have to subscribe (on the client side, profile.js) to this publication 
+// Publication of the "points" field of the users. Now we have to subscribe (on the client side, profile.js) to this publication
 Meteor.publish('userData', function () {
   if (this.userId) {
     return Meteor.users.find({}, {
-      fields: { username: 1, points: 1 }
+      fields: { username: 1, points: 1, colocName : 1 }
     });
   } else {
     this.ready();
