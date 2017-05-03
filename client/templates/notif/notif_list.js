10e277fe87d03c85fbc3352f7d48d3e533eb7a87
Leonardo <leonardo.c@hotmail.frleonardo.c@hotmail.frleonardo.c@hotmail.frleonardo.c@hotmail.fr>
2017-05-03 09:59:23 +0200

    arrangement de la liste de notif et ajout en dernière position

--- client/templates/notif/notif_list.js
+++ client/templates/notif/notif_list.js
@@ -1,21 +1,22 @@
 // Helpers for the notifList template. Returns every notif from the database and assign it to "notif"
 Template.notifList.events({
-	
+
 	'click .addTache': function() {
 
 					var contenu = prompt('Entrez le nom de la tâche');
 
 					var newTache = document.createElement('div');
 					newTache.id = 'nouvelleTache';
+					newTache.className = 'notif';
 
 					newTache.innerHTML = `<h4 class='notifTexte'>${contenu}</h4>`;//comment faire sans utiliser css ?
 
 					document.body.appendChild(newTache); //insère l'objet en tant que dernier éléments => a la fin
 
 
-					var derniereNotif = document.getElementById('derniereNotif');
+					var derniereNotif = document.querySelector('.notif');
 					var element_parent = document.getElementById("toutes_notif");
 
-					element_parent.insertBefore(newTache, derniereNotif);				
+					element_parent.insertBefore(newTache, derniereNotif);			
 	}
 });
