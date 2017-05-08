c0686566e1f443b3dda5baf6dcf24633ffe6240c
ptalon91 <p.talon@bluewin.ch>
2017-05-08 14:35:05 +0200

    Confirmation box now pops when clicking on a task
    User has to confirm that he actually did the task. If yes, points are
    addedd, notification is created and he's redirected to coloc page. If
    no, he stays on the tasks page.

--- client/templates/tasks/tasks_list.js
+++ client/templates/tasks/tasks_list.js
@@ -10,19 +10,43 @@ Template.tasksList.helpers({
 // For current user, and for the clicked task's number of points.
 Template.tasksList.events({
 	'click .task-content': function(){
-	    Meteor.call(
-	    	'incrementPoints',
-	    	Meteor.userId(),
-	    	this.points
-	    );
+	    
+		// Parameters...
+		let task_points = this.points;
+		let task_descr = this.descr;
+		let box_message = "Confirmez-vous avoir " + task_descr + " ?";
+		
+		// Confirmation box when clicking a task. "ok" is true if the user clicked on "ok", false otherwise
+		new Confirmation({
+		  message: box_message,
+		  title: "Confirmation",
+		  cancelText: "Annuler",
+		  okText: "Oui",
+		  success: true, // whether the button should be green or red
+		  focus: "cancel" // which button to autofocus, "cancel" (default) or "ok", or "none"
+		}, function (ok) {
+		
+			if (ok == true){
+				// Call method on server for incrementing user's points.
+				Meteor.call(
+					'incrementPoints',
+					Meteor.userId(),
+					task_points
+				);
 
-	    Meteor.call(
-	    	'createNotif',
-	    	Meteor.userId(),
-	    	Meteor.user().username,
-	    	Meteor.user().points,
-	    	this.points,
-	    	this.descr
-		);
+				// Call method on server to create a notification.
+				Meteor.call(
+					'createNotif',
+					Meteor.userId(),
+					Meteor.user().username,
+					Meteor.user().points,
+					task_points,
+					task_descr
+				);
+				
+				// After confirmation, redirect to coloc page.
+				Router.go('coloc');
+			}
+		});
 	}
 });
--- server/tasksMethods.js
+++ server/tasksMethods.js
@@ -1,19 +1,19 @@
 // Methods called in tasks_list.js.
-
-// Updates current user's number of points with the clicked task's number of points.
 Meteor.methods({
-  incrementPoints(current_userId, task_points) {
-    Meteor.users.update({_id: current_userId}, {$inc: {'points': task_points}})
-  }
+	
+	// Updates current user's number of points with the clicked task's number of points.
+	incrementPoints(current_userId, task_points) {
+		Meteor.users.update({_id: current_userId}, {$inc: {'points': task_points}})
+	},
+
+	// Creates a notification when user clicks on a task he made.
+    createNotif(current_userId, current_username, current_username_points, task_points, task_descr) {
+		Notifs.insert({
+			text: current_username + " a " + task_descr + " et remporte " + task_points + " points. Bravo !",
+			authorID: current_userId,
+			createdAt: new Date()
+		})
+	}
 });
 
-// Creates a notification when user clicks on a task he made.
-Meteor.methods({
-  createNotif(current_userId, current_username, current_username_points, task_points, task_descr) {
-    Notifs.insert({
-    	text: current_username + " a " + task_descr + " et remporte " + task_points + " points. Bravo !",
-    	authorID: current_userId,
-    	createdAt: new Date()
-    })
-  }
-});
+
