c8ff189ef4bbb925719855dba30462bd77cc6d52
nvallott <nvallott@hotmail.com>
2017-05-11 15:48:21 +0200

    modifs sur create_coloc

--- client/templates/create_coloc/create_coloc.js
+++ client/templates/create_coloc/create_coloc.js
@@ -1,9 +1,7 @@
 Template.createColoc.events({
 	'keypress .createColoc': function(evt){
-
-      if (evt.which === 13){
-      let contenu = document.getElementById('colocName').value;
-
+			let contenu = document.getElementById('colocName').value;
+      if (evt.which === 13 && contenu !== ''){
       Meteor.call(
         'createColoc',
         Meteor.userId(),
